84d0a3d7b7873450f0307cc46644b2931b5b6f07
Dhieu <dhieumajok211@gmail.com>
2024-12-09 13:18:37 +0300

    added the app function and the menu for the respondent

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from sms_service import SMS
-from models import WastePickup
 
 app = Flask(__name__)
 
@@ -10,15 +9,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///waste_management.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+
 
 # Create the database and tables
 with app.app_context():
     db.create_all()
 
 # Initialize Africa's Talking with environment variables
-username = os.getenv('AFRICASTALKING_USERNAME', 'YOUR USER NAME HERE')
-api_key = os.getenv('AFRICASTALKING_API_KEY', 'YOUR API KEY HERE')
+username = os.getenv('AFRICASTALKING_USERNAME', 'davidas')
+api_key = os.getenv('AFRICASTALKING_API_KEY', 'atsk_770cdbd4bfcc606a586c95d34c13e5576b07bb7ffe3fc04f43fad4e4882130bbb869415f')
 
 sms_service = SMS(username, api_key)
 
@@ -55,3 +54,149 @@ def ussd():
 
     # Extract the latest input part for each step
     latest_input = text.split('*')[-1]
+
+    if text == '':
+        response = "CON Welcome to Pick Waste Solutions:\n"
+        response += "1) Residential Waste Collection\n"
+        response += "2) Commercial Waste Collection\n"
+        response += "3) Construction and Demolition Waste\n"
+        response += "4) Hazardous Waste Disposal\n"
+        session['step'] = 1
+    elif session['step'] == 1:
+        options = {
+            '1': 'Residential Waste Collection',
+            '2': 'Commercial Waste Collection',
+            '3': 'Construction and Demolition Waste',
+            '4': 'Hazardous Waste Disposal'
+        }
+
+        if latest_input in options:
+            session['waste_service'] = options[latest_input]
+            current_pickup.waste_type = session['waste_service']  # This line save the service type here when a user enter data
+            db.session.commit()
+            response = f"CON {session['waste_service']} selected.\n"
+            response += "Choose Waste Type:\n"
+            response += "1) Solid Waste\n"
+            response += "2) Liquid Waste\n"
+            session['step'] = 2
+        else:
+            response = "END Invalid option. Please try again."
+            session['step'] = 0
+            return response
+    elif session['step'] == 2:
+        waste_types = {
+            '1': 'Solid Waste',
+            '2': 'Liquid Waste'
+        }
+
+        if latest_input in waste_types:
+            session['waste_type'] = waste_types[latest_input]
+            current_pickup.waste_type = session['waste_type']
+            db.session.commit()
+            response = "CON Enter Address to Pick the Waste:\n"
+            session['step'] = 3
+        else:
+            response = "CON Invalid option. Please select the waste type:\n"
+            response += "1) Solid Waste\n"
+            response += "2) Liquid Waste\n"
+    elif session['step'] == 3:
+        session['address'] = text.lstrip('1234567890* ')
+        current_pickup.address = session['address']
+        db.session.commit()
+        response = "CON Select Pickup Day:\n"
+        response += "1) Monday\n"
+        response += "2) Tuesday\n"
+        response += "3) Wednesday\n"
+        response += "4) Thursday\n"
+        response += "5) Friday\n"
+        response += "6) Sunday\n"
+        session['step'] = 4
+    elif session['step'] == 4:
+        days = {
+            '1': 'Monday',
+            '2': 'Tuesday',
+            '3': 'Wednesday',
+            '4': 'Thursday',
+            '5': 'Friday',
+            '6': 'Sunday'
+        }
+
+        if latest_input in days:
+            session['pickup_day'] = days[latest_input]
+            current_pickup.pickup_day = session['pickup_day']
+            db.session.commit()
+            response = f"CON Pickup day set: {session['pickup_day']}\n"
+            response += "Select Pickup Time:\n"
+            response += "1) 8:00 AM - 10:00 AM\n"
+            response += "2) 10:00 AM - 12:00 PM\n"
+            response += "3) 12:00 PM - 2:00 PM\n"
+            response += "4) 2:00 PM - 4:00 PM\n"
+            response += "5) 4:00 PM - 6:00 PM\n"
+            session['step'] = 5
+        else:
+            response = "CON Invalid option. Please select a pickup day:\n"
+            response += "1) Monday\n"
+            response += "2) Tuesday\n"
+            response += "3) Wednesday\n"
+            response += "4) Thursday\n"
+            response += "5) Friday\n"
+            response += "6) Sunday\n"
+    elif session['step'] == 5:
+        times = {
+            '1': '8:00 AM - 10:00 AM',
+            '2': '10:00 AM - 12:00 PM',
+            '3': '12:00 PM - 2:00 PM',
+            '4': '2:00 PM - 4:00 PM',
+            '5': '4:00 PM - 6:00 PM'
+        }
+
+        if latest_input in times:
+            session['pickup_time'] = times[latest_input]
+            current_pickup.pickup_time = session['pickup_time']
+            db.session.commit()
+
+            detailed_message = (
+                f"Your waste pickup request was sent successfully.\n"
+                f"Details are as follows:\n"
+                f"{session['waste_service']}\n"
+                f"Type of Waste: {session['waste_type']}\n"
+                f"Your Address: {session['address']}\n"
+                f"Pickup day: {session['pickup_day']}\n"
+                f"Pickup time: {session['pickup_time']}\n"
+                f"Thank you for using our services! Customer Care: +256787927092, +211925413115\n"
+                f"Pick Waste Solutions. Empowering Communities for a Cleaner Tomorrow"
+            )
+
+            # Send SMS to the dynamic phone number
+            sms_service.send(detailed_message, [phone_number])
+
+            response = "END Your Waste Pickup request has been submitted. Thank you for using our services!"
+            user_sessions.pop(session_id, None)
+        else:
+            response = "CON Invalid option. Please select a pickup time:\n"
+            response += "1) 8:00 AM - 10:00 AM\n"
+            response += "2) 10:00 AM - 12:00 PM\n"
+            response += "3) 12:00 PM - 2:00 PM\n"
+            response += "4) 2:00 PM - 4:00 PM\n"
+            response += "5) 4:00 PM - 6:00 PM\n"
+    else:
+        response = "END Invalid input. Please try again."
+
+    return response
+
+@app.route("/view_data", methods=['GET'])
+def view_data():
+    pickups = WastePickup.query.all()
+    results = [
+        {
+            "phone_number": pickup.phone_number,
+            "waste_type": pickup.waste_type,
+            "address": pickup.address,
+            "pickup_day": pickup.pickup_day,
+            "pickup_time": pickup.pickup_time
+        } for pickup in pickups]
+
+    return jsonify({"pickups": results}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
