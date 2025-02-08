3ab1d52610d454876a42282d1f3ab8d99b39812d
Dhieu <112936899+Dhie-boop@users.noreply.github.com>
2025-02-08 23:39:17 +0300

    Update sms_service.py

--- sms_service.py
+++ sms_service.py
@@ -17,13 +17,6 @@ class SMS:
             print(f'Encountered an error while sending: {e}')
             return None
 
-if __name__ == '__main__':
-    # Initialize the SMS service with your credentials
-    username = "davidas"  
-    api_key = "atsk_770cdbd4bfcc606a586c95d34c13e5576b07bb7ffe3fc04f43fad4e4882130bbb869415f"
-    sms_service = SMS(username, api_key)
 
-    # Example message to be sent
-    message = "Thank you for using our services!"
 
-    # Recipients' phone numbers will be dynamically handled in the Flask app
+    
